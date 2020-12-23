b57238cca311ef53d94759ba11d8f88290a0af09
Mohammad Tauqir <tauqir295@gmail.com>
2020-12-23 20:31:36 +0530

    basic configuration changes

---
+++ mvvm-retrofit/src/main/java/com/sample/mvvm/retrofit/utils/Logger.kt
@@ -0,0 +1,26 @@
+package com.sample.mvvm.retrofit.utils
+
+import android.util.Log
+import com.sample.mvvm.retrofit.BuildConfig
+
+object Logger {
+
+    /**
+     * check debug flavor
+     */
+    private fun enableLog():Boolean {
+        return BuildConfig.DEBUG
+    }
+
+    fun d (tag:String, msg:String) {
+        when {
+            enableLog() -> Log.d(tag, msg)
+        }
+    }
+
+    fun e (tag:String, msg:String) {
+        when {
+            enableLog() -> Log.d(tag, msg)
+        }
+    }
+}
