e2147d9af41472a83a6dcea35abb54599a0b468d
Mohammad Tauqir <tauqir295@gmail.com>
2021-01-20 21:57:36 +0530

    crash fix

--- mvvm-retrofit/src/main/java/com/mvvm/retrofit/ui/CatLibLandingViewModel.kt
+++ mvvm-retrofit/src/main/java/com/mvvm/retrofit/ui/CatLibLandingViewModel.kt
@@ -1,12 +1,16 @@
 package com.mvvm.retrofit.ui
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mvvm.retrofit.network.model.Cat
 import com.mvvm.retrofit.network.repository.MainRepository
 import com.mvvm.retrofit.utils.NetworkHelper
 import com.mvvm.retrofit.utils.Resource
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 class CatLibLandingViewModel @ViewModelInject constructor(
     private val mainRepository: MainRepository,
@@ -21,15 +25,20 @@ class CatLibLandingViewModel @ViewModelInject constructor(
         fetchCatList()
     }
 
-    private fun fetchCatList(limit:Int = 20, page:Int = 1) {
+    private fun fetchCatList(limit: Int = 20, page: Int = 1) {
         viewModelScope.launch {
             _cats.postValue(Resource.loading(null))
             if (networkHelper.isNetworkConnected()) {
-                mainRepository.getCats(limit, page).let {
-                    if (it.isSuccessful) {
-                        _cats.postValue(Resource.success(it.body()))
-                    } else _cats.postValue(Resource.error(it.errorBody().toString(), null))
+                try {
+                    mainRepository.getCats(limit, page).let {
+                        if (it.isSuccessful) {
+                            _cats.postValue(Resource.success(it.body()))
+                        } else _cats.postValue(Resource.error(it.errorBody().toString(), null))
+                    }
+                } catch (e: IOException) {
+                    _cats.postValue(Resource.error("No internet connection", null))
                 }
+
             } else _cats.postValue(Resource.error("No internet connection", null))
         }
     }
