e4a708ed02c9b06fa1189654b5b0c7986457369c
Mohammad Tauqir <tauqir295@gmail.com>
2021-01-21 00:31:59 +0530

    code-cleanup

--- mvvm-retrofit/src/main/java/com/mvvm/retrofit/di/CustomModule.kt
+++ mvvm-retrofit/src/main/java/com/mvvm/retrofit/di/CustomModule.kt
@@ -16,6 +16,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 @InstallIn(ApplicationComponent::class)
@@ -28,6 +29,9 @@ object CustomModule {
             val loggingInterceptor = HttpLoggingInterceptor()
             loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
             OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
                 .addInterceptor(loggingInterceptor)
                 .build()
         } else OkHttpClient
--- mvvm-retrofit/src/main/java/com/mvvm/retrofit/ui/CatLibLandingFragment.kt
+++ mvvm-retrofit/src/main/java/com/mvvm/retrofit/ui/CatLibLandingFragment.kt
@@ -6,15 +6,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.SwitchCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.appbar.MaterialToolbar
 import com.mvvm.retrofit.R
 import com.mvvm.retrofit.databinding.LandingFragmentBinding
 import com.mvvm.retrofit.network.model.Cat
@@ -59,9 +58,9 @@ class CatLibLandingFragment : Fragment(), CatRecyclerViewAdapter.OnRecyclerItemC
         super.onViewCreated(view, savedInstanceState)
 
         setupObserver()
-        requireActivity().findViewById<TextView>(R.id.toolbarTitle).text = getString(R.string.cat_list)
 
-        requireActivity().findViewById<ImageView>(R.id.toolbarImage).setOnClickListener {
+        requireActivity().findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener {
+            // Handle navigation icon press
             requireActivity().onBackPressed()
         }
 
